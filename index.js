c7f7067e0d2f9d150b422523006ca6ca19648491
shalom-nganga <minnomacharia@gmail.com>
2023-10-12 12:28:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+const name = (["Guadalupe", "Ollie", "Aki"])
+function writeCards(name) {
+ const thankYouMesssages = [] 
+  for (let x = 0; x < name.length; x++) {
+    const message = `Thank you, ${name[x]}, for the wonderful surprise gift!`
+    thankYouMesssages.push(message)
+  }
+  return thankYouMesssages
+ }
+
+ function countDown(number) {
+  console.log(number);
+  let x = number -1
+  while (x >= 0) {
+    console.log(x)
+    x--
+  }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
